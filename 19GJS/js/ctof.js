9f799d8adb9911035a27af09bcceadd3b12d74b9
LennyCocom <lennyinnelcocompedraza@gmail.com>
2022-05-18 19:29:50 -0500

    second

---
+++ 19GJS/js/ctof.js
@@ -0,0 +1,9 @@
+//convertir los grados celsius to farentheit
+//Imprimir en consola
+
+
+let gradosCelsius = prompt ('Digita los grados celsius que se encuentra en tu localidad')
+
+let gradosF = gradosCelsius * 9/5 + 32
+
+console.log( gradosF)
