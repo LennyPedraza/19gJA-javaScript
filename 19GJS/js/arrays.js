7da6ec4ce4d3ea4cba544d055d295420921fefc6
LennyCocom <lennyinnelcocompedraza@gmail.com>
2022-05-26 20:36:11 -0500

    arrays

--- 19GJS/js/arrays.js
+++ 19GJS/js/arrays.js
@@ -28,7 +28,7 @@
 //     arrayToReverse.push(arrayToReverse[i])
 // }
 
-let newArray = [ 1, 2, 3, 'str', [1,2] ]
+// let newArray = [ 1, 2, 3, 'str', [1,2] ]
 // console.log(newArray)
 // .length -> tamaño del array -> cantidad de elementos que contiene
 // 
@@ -39,23 +39,23 @@ let newArray = [ 1, 2, 3, 'str', [1,2] ]
 // for(i = 0; i < newArray.length ; i++ ){
 //     console.log(newArray[i])
 // }
-let arrayToReverse = [1,2,3]
+// let arrayToReverse = [1,2,3]
 // -> [1,2,3] -> [3,2,1]
-let arrReversed = []
-for( i = arrayToReverse.length - 1; i >= 0; i-- ){
-    arrReversed.push( arrayToReverse[i] )
-}
+// let arrReversed = []
+// for( i = arrayToReverse.length - 1; i >= 0; i-- ){
+//     arrReversed.push( arrayToReverse[i] )
+// }
 
-console.log(arrayToReverse, arrReversed)
+// console.log(arrayToReverse, arrReversed)
 
-arrReversed.push(3)
-arrReversed.push(3)
-arrReversed[0] = 10
-console.log(arrReversed)
-arrReversed[5] = 100
-console.log(arrReversed)
-arrReversed[ arrReversed.length ] = 1000
-console.log(arrReversed)
+// arrReversed.push(3)
+// arrReversed.push(3)
+// arrReversed[0] = 10
+// console.log(arrReversed)
+// arrReversed[5] = 100
+// console.log(arrReversed)
+// arrReversed[ arrReversed.length ] = 1000
+// console.log(arrReversed)
 
 
 
@@ -63,3 +63,69 @@ console.log(arrReversed)
 // arr.push(item)
 // .push() -> agregar nuevo item al array al final
 // .pop() -> Elimina nuevo item al array
+
+
+// forEach()
+
+// let arr = [1,2,3,4]
+// for (i=0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+// arr.forEach((cv,index,arr) => {
+
+//     console.log(cv)
+// })
+
+
+
+/**
+ * Escribir una funcion
+ * Reciba como parametro Un array de ciudades
+ * Retornar las ciudades capitaslizadas
+ * capitalize(['méxicO','RIo', 'Los AngelEs'])
+ * -> ['México','Rio', 'Los Angeles']
+ * 
+ */
+
+
+
+//  const capitalize =ciudades.toLowerCase()
+//  console.log(capitalize)
+
+const str = ['méxicO', 'RIo', 'Los AngelEs']
+
+const capitalize = (str) => {
+    let arr = []
+
+    str.forEach(element => {
+
+        const lower = element.toLowerCase();
+        let strCap = element.charAt(0).toUpperCase() + lower.slice(1);
+        arr.push(strCap)
+
+    });
+    return arr
+
+}
+capitalize(str)
+
+
+let arrStr = ['hola','mundo']
+/**
+ * Funcion
+ * getFirstLast(['hola','mundo'])
+ * -> ['ha','mo']
+ * .map()
+ */
+let result = arrStr.map((str,index,cn) =>{
+    return arrStr.charAt[0] // name.[0] name.slice[0]
+    
+} )
+
+
+
+
+
+ 
+
--- 19GJS/js/homework.js
+++ 19GJS/js/homework.js
@@ -1,80 +1,52 @@
 /**
- * Ejercicio 1 *Obligatorio
- * Realizar una funcion que reciba como parametro 1 array
- * Y devuelva un array con solo los elementos Pares de ese array
- * p.ej.
- * -> evenOnly( [1,2,3,4,18] ) -> [2,4,18]
- * -> evenOnly( [10, 2, 5] ) -> [10,2]
- * -> evenOnly() -> 'Se necesita un array'
+ * Funcion que reciba una palabra
+ * Retorne la palabra invertida
+ *
+ * reverseStr('hola mundo')
+ * -> 'odnum aloh'
  * 
+ * Usar solo metodos de str y array
  */
-const isEven = () => {
-let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
-let arrayEven= []
-for(i= 0; i>= array.length; i++){
-    if (array[i] % 2 === 0 ) {
-        arrayEven.push(array[i])
-        console.log(isEven)
-    } 
-}
-}
+
+// const reversedStr = (str) =>{
+//     let newStr= str.split('').reverse().join('') 
+// return newStr
+// }
+
+// el codigo al tener  se puede reducir
+const reversedStr = str => {return str.split('').reverse().join('') }
 
 /**
- * Ejercicio 2 *Opcional
- * Funcion que reciba como parametro una array de strings
- * y devuelva el primer y ultimo caracter de cada string
- * p.ej.
- * -> firstAndLast ( ['hola', 'mundo'] ) -> ['ha', 'mo']
+ * Funcion que reciba un array de numeros
+ * y devuelva el promedio de todos los elementos
+ * 
+ * getAverage( [10, 8, 9, 7] )
+ * -> 8.5
+ */
+
+// const average = (list) =>{
+
+//     const sumList = list.reduce(function(value = 0,newElement))
+
+
+// }
+
+
+
+
+
+
+/**
+ * Funcion que reciba un array de strings
+ * y devuelva un array indicando si son palindromos o no
+ * 
+ * arePalindrome( ['oso','juan', 'seres'] )
+ * -> [true, false, true]
  */
 
 
 /**
- * Ejercicio 3
- * Estudiar todos los métodos de Array
- * https://www.w3schools.com/jsref/jsref_obj_array.asp
- * https://www.programiz.com/javascript/library/array
- * 
+ * Estudiar los métodos de array
+ * .filter()
+ * .reduce()
  */
-// concat()
-// const array1 = ["tacos", "hamburgesa"]
-// const array2 = ["soda", "cerveza", "agua"]
-
-// const arrayConcat = array1.concat(array2)
-
-// console.log(arrayConcat)
-
-// // constructor Property: returns that created  the Array portotype
-// const fruits = ["Banana", "Orange", "Apple", "Mango"];
-// let text = fruits.constructor;
-// console.log(text)
-
-
-// copyWithin
-
-const numeros = [1,2,3,4,5,6,7]
-
-// console.log(numeros)
-
-numeros.copyWithin (3,2)
-
-// console.log(numeros)
-
-numeros.copyWithin(2,3,5)
-
-console.log(numeros)
-
-
-// entries()
-
-// const refrescos = ['coca-cola','jarritos',]
-
-// const refrescos2 = refrescos.entries()
-
-// console.log(refrescos2.next().value)
-// console.log(refrescos2.next().value)
-
-
-
-
-
-
