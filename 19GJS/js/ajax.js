0104e0efe019bb7f48478a368c59d7e9598508bc
LennyCocom <lennyinnelcocompedraza@gmail.com>
2022-06-10 20:06:18 -0500

    post

--- 19GJS/js/ajax.js
+++ 19GJS/js/ajax.js
@@ -1,74 +1,148 @@
-// Ajax
-// Asyncronic JavaScaript And XML
-// XMLHttpRequest
 
+//GET
+//Verbos http
+//payload
 
-/**
-    Holds the status of the XMLHttpRequest.
-    0: request not initialized
-    1: server connection established
-    2: request received
-    3: processing request
-    4: request finished and response is ready
- */
+//CREATE  POST
+//Read  GET
+//Update PATCH - PUT
+//Delete DELETE
 
+// const createUser = (objUser) =>{
+//     console.log(objUser)
+//     const xhttp = new XMLHttpRequest()
+//     console.log(xhttp)
+//     xhttp.open("POST","",true)
+//     xhttp.onload= function (data){
+//         if(data.target.statis >= 200 && data.target.status <= 399)
+//         {
+//             console.log(data)
+//         }
+//   }
+// //JSON.parse(str) -> ob
+// //JSON.string(obj) -> texto
 
-// formar request
-// enviar request
-// esperar respuesta
-// recibir response
-// procesar el response
+//     xhttp.send(JSON.stringify)
+// }
 
-const printUsers = (arrObjs) => {
-    let template = arrObjs.reduce((acc, user,) => {
-        return acc += `
-            <li>
-                ${user.id}: ${user.title} 
-                <p>${user.body}</p>
-            </li>
-        `
-    }, '')
-    document.querySelector('#lista__users').innerHTML = template
-}
+// let user = {
+//     name:'Jorge Luis',
+//     lastName :'Camarillo',
+//     age:31
+// }
 
-// Create an XMLHttpRequest object
-const primerPeticion = new XMLHttpRequest()
+// createUser(user)
 
-let users = []
+// let text =JSON.stringify(objeto)  para convertir un objeto a texto
 
-// Define a callback function
-primerPeticion.onload = (response) => {
-    // Here you can use the Data
-    console.log('REspuesta lista')
-    console.log(response.target)
+// JSON.parse(texto)  para convertir un texto plano a objeto
+// const createUser =  ( objUser ) => {
+//     console.log(JSON.stringify(objUser))
+//     const xhttp = new XMLHttpRequest()
+//     xhttp.open( "POST",  
+//       "https://koders19gjs-default-rtdb.firebaseio.com/users/.json", true)
+//     xhttp.onload = function(data) {
+//       console.log(data)
+//         if(data.target.status >= 200 && data.target.status <= 399){
+//             console.log(data.target.response)
+//         }
+//     }
+//     xhttp.send( JSON.stringify(objUser) )
+//   }
+  
 
-    if(response.target.status >= 200 && response.target.status <= 299) {
-        // la respuesta fue satisfactoria
-        console.log( response.target.responseText )
-        // JSON
-        // metodos 
-        // .parse( text - Object ) 
-        // .stringify( obj - text)
+//   createUser(user)
 
-        let responseJSON = JSON.parse(response.target.responseText)
-        users = responseJSON
-        printUsers(users)
-    }
-}
+const createUser =  ( objUser ) => {
+        console.log(JSON.stringify(objUser))
+        const xhttp = new XMLHttpRequest()
+        xhttp.open( "POST",  
+          "https://lennyfirebase-93904-default-rtdb.firebaseio.com/.json", true)
+        xhttp.onload = function(data) {
+          console.log(data)
+            if(data.target.status >= 200 && data.target.status <= 399){
+                console.log(data.target.response)
+            }
+        }
+        xhttp.send( JSON.stringify(objUser) )
+      }
+      
+  let user =  [{
+        name: 'Lenny Innel',
+        lastName: 'Cocom',
+        age: 26
+      },
+      {
+        name: 'Luis',
+        lastName: 'Perez',
+        age: 26
+      },
+      {
+        name: 'Jorge',
+        lastName: 'Sanchez',
+        age: 29
+      },
+      {
+        name: 'Brenda',
+        lastName: 'Gutierrez',
+        age: 37
+      },
+      {
+        name: 'Claudia',
+        lastName: 'Quiroz',
+        age: 44
+      },
+      {
+        name: 'Mariana',
+        lastName: 'Rodriguez',
+        age: 18
+      },
+      {
+        name: 'Lorena',
+        lastName: 'Martinez',
+        age: 25
+      },
+      {
+        name: 'Jaime',
+        lastName: 'Cocom',
+        age: 59
+      },
+      {
+        name: 'Jorge',
+        lastName: 'Camarillo',
+        age: 72
+      },
+      {
+        name: 'Veronia',
+        lastName: 'Camarillo',
+        age: 36
+      },
+      {
+        name: 'Juan',
+        lastName: 'Salinas',
+        age: 43
+      }
+    ]
+ 
+    // user.forEach((user)=>{
+    //     createUser(user)
+    // })
+  
 
-// Send a request
-// GET, POST, DELETE, PATCH, PUT
-primerPeticion.open("GET", "https://jsonplaceholder.typicode.com/posts", false)
-// console.log(primerPeticion)
-primerPeticion.send()
-// console.log(primerPeticion)
-
-
-
-
-
-
-
-
-
-
+    // const deleteUser  = ( idUser ) => {
+    //     const deletexhr = new XMLHttpRequest()
+    //     deletexhr.open(
+    //       'DELETE', 
+    //       `https://koders19gjs-default-rtdb.firebaseio.com/users/${idUser}.json`, 
+    //       true
+    //     )
+    //     deletexhr.onload = (response) => {
+    //       if(response.target.status >= 200 && response.target.status <= 399){
+    //         console.log(response.target)
+    //         console.log(response.target.response)
+    //       }
+    //     }
+    //     deletexhr.send()
+    //   }
+      
+    //   deleteUser('-N4AGluLtPc1l2_3tKTS')
